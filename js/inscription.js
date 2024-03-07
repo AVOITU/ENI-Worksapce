ebf4b21b09ff0739343441b7a45b095c35172a3e
Antoine VOITURON <antoine.voituron2024@campus-eni.fr>
2024-03-07 14:08:19 +0100

    add json file to create usersdatabase fectch it with inscription.js

--- js/inscription.js
+++ js/inscription.js
@@ -30,11 +30,17 @@ function localUsers() {
 
     e.preventDefault();
 
+    fetch('usersData.json')
+    .then(reponse => reponse.json())
+    .then(data => storeData(data))
+    .then(() => afficher(getData()))
+
     const infoUser = ["nomUtilisateur", "email", "mdp", "passwordVerification"];
     infoUser.forEach((element) => {
       let getInput = document.getElementById(element).value;
       localStorage.setItem(element, getInput);
       let newElement = localStorage.getItem(element);
+
       
     });
   }
