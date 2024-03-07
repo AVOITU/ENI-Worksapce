a493eab35b08067564133b8b0d640313d6c086ca
Antoine VOITURON <antoine.voituron2024@campus-eni.fr>
2024-03-07 09:26:32 +0100

    comments suppression

--- js/inscription.js
+++ js/inscription.js
@@ -1,10 +1,3 @@
-// avec un niveau de difficulté du mot de passe faible, moyen, fort.
-// Règles : si le mot de passe contient moins de 6 caracteres il est faible.
-// S'il contient plus de 6 caracteres avec un symbole ou un nombre il est moyen.
-// S'il contient plus de 9 caracteres avec un symbole et un nombre il est fort.
-
-// Mettre en place une écoute sur le mdp
-
 function mdpSecurity() {
   
   const inputPassword = document.getElementById("mdp");
