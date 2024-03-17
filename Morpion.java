c31b75dd09d9ca06d7f72451a87e1aab98e8b6bf
Antoine Voituron <avoitu@orange.fr>
2024-03-17 19:38:14 +0100

    adding java tictactoe game for personnal training

---
+++ Morpion.java
@@ -0,0 +1,89 @@
+package fr.eni.morpion;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Morpion {
+	public static void main(String[] args) {
+		//String[][] tableauJeu = new String[3][3];
+		String[][] tableauJeu = {
+			    {"X", "X", "X"},
+			    {"X", "X", "X"},
+			    {"X", "X", "1"}
+			};
+		char player = '1';
+		System.out.println(Arrays.deepToString(tableauJeu));
+		for (int i = 0; i < 8; i++) {
+			String inputCross = posCrossInput();
+			int[] positions = vericationsPosition(inputCross);
+			putACross(positions, tableauJeu);
+			player = togglePlayer(player);
+			putANought(positions, tableauJeu);
+			player = togglePlayer(player);
+		}
+	}
+
+	public static String posCrossInput() {
+		System.out.println("Entrer la ligne et la colonne de votre croix sous forme : no_ligne,no_colonne");
+		Scanner posCross = new Scanner(System.in);
+		String inputCross = posCross.nextLine();
+		return inputCross;
+	}
+
+	public static int[] vericationsPosition(String inputCross) {
+		String separation[] = inputCross.split(",");
+		if (separation.length != 2) {
+			System.out.println("Erreur vous n'avez pas rentré le bon nombre de chiffres");
+			posCrossInput();
+		}
+
+		int[] integerPositions = transformInputInInteger(separation);
+		if (integerPositions[0] > 2 || integerPositions[0] < 0 || integerPositions[1] < 0 || integerPositions[1] > 2) {
+			System.out.println("Erreur : vous souhaitez mettre une croix en dehors de la grille de morpion?!!");
+			posCrossInput();
+		}
+
+		return new int[] { integerPositions[0], integerPositions[1] };
+	}
+
+	public static int[] transformInputInInteger(String[] separation) {
+		int positionLine = Integer.parseInt(separation[0]) - 1;
+		int positionColumn = Integer.parseInt(separation[1]) - 1;
+		return new int[] { positionLine, positionColumn };
+	}
+
+	public static void putACross(int[] positions, String[][] tableauJeu) {
+		int positionLine = positions[0];
+		int positionColumn = positions[1];
+		tableauJeu[positionLine][positionColumn] = "X";
+		System.out.println(Arrays.deepToString(tableauJeu));
+	}
+
+	public static char togglePlayer(char player) {
+		if (player == '1') {
+			player = '2';
+			System.out.println("Le joueur 2 dessine un rond");
+		} else {
+			player = '1';
+			System.out.println("A votre tour");
+		}
+
+		return player;
+	}
+
+	public static void putANought(int[] positions, String[][] tableauJeu) {
+		int randomLine = (int) (Math.random() * 2);
+		int randomColumn = (int) (Math.random() * 2);
+		
+		while (tableauJeu[randomLine][randomColumn]!="X"||tableauJeu[randomLine][randomColumn]!="O") {
+			randomLine = (int) (Math.random() * 2);
+			randomColumn = (int) (Math.random() * 2);
+		}
+		tableauJeu[randomLine][randomColumn] = "O";
+		System.out.println(Arrays.deepToString(tableauJeu));
+	}
+	
+	public static void checkVictory() {
+		
+	}
+}
