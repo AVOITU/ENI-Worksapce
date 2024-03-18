2032eb173ba181a8d43928589ba38f01bf680376
Antoine VOITURON <antoine.voituron2024@campus-eni.fr>
2024-03-18 09:01:46 +0100

    adding new local repository

---
+++ Morpion.java
@@ -0,0 +1,23 @@
+package fr.eni.morpion;
+
+import java.util.Arrays;
+import java.util.function.Function;
+
+import javax.print.DocFlavor.INPUT_STREAM;
+
+public class Morpion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//écrire syso puis appuyé sur ctrl + espace
+		String [][] tableauJeu = {};
+		System.out.println(Arrays.toString(tableauJeu));
+		cross("je suis dans la fonction");
+	}
+		
+		void cross(String tableauJeu) {
+			System.out.println("je suis dans la fonction");
+		}
+	}
+
+}
